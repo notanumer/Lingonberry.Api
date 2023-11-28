7820f0742549195881ed293af079a147e904202e
Galimzyanov Ainur <a.galimz@mail.ru>
2023-11-28 16:57:13 +0500

    Create employee controller.

---
+++ src/Lingonberry.Api.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Lingonberry.Api.UseCases.Employee.GetEmployeesByCity;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lingonberry.Api.Web.Controllers;
+
+/// <summary>
+///
+/// </summary>
+[ApiController]
+[Route("api/employee")]
+[ApiExplorerSettings(GroupName = "employee")]
+public class EmployeeController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public EmployeeController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    /// <summary>
+    /// Get employees by city.
+    /// </summary>
+    /// <param name="query">GetEmployeesByCityQuery.</param>
+    /// <returns>GetEmployeesByCityResult.</returns>
+    [HttpGet("getEmployeesByCity")]
+    public async Task<GetEmployeesByCityResult> GetEmployeesByCity([FromQuery] GetEmployeesByCityQuery query)
+    {
+        return await mediator.Send(query);
+    }
+}
