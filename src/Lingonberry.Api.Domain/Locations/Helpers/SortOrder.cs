fb85b848e838a8073aae1f44d7f721c152537951
Galimzyanov Ainur <a.galimz@mail.ru>
2023-12-01 21:23:47 +0500

    Add enum for filtering

---
+++ src/Lingonberry.Api.Domain/Locations/Helpers/SortOrder.cs
@@ -0,0 +1,17 @@
+﻿namespace Lingonberry.Api.Domain.Locations.Helpers;
+
+/// <summary>
+/// Enum for defining filtering from a to z or from z to aю
+/// </summary>
+public enum SortOrder
+{
+    /// <summary>
+    /// Ascending.
+    /// </summary>
+    Ascending,
+
+    /// <summary>
+    /// Descending.
+    /// </summary>
+    Descending
+}
---
+++ src/Lingonberry.Api.Domain/Users/Gender.cs
@@ -0,0 +1,17 @@
+﻿namespace Lingonberry.Api.Domain.Users;
+
+/// <summary>
+/// User gender.
+/// </summary>
+public enum Gender
+{
+    /// <summary>
+    /// Male
+    /// </summary>
+    Male = 1,
+
+    /// <summary>
+    /// Female
+    /// </summary>
+    Female = 2
+}
