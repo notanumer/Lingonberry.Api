83298f5540e286d8be915c896d966f5365308f2e
Nikita Kolchin <n.kolchin@smartway.today>
2023-12-03 13:56:03 +0300

    Add position for user

---
+++ src/Lingonberry.Api.Infrastructure.DataAccess/Migrations/20231203100755_AddUserPosition.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lingonberry.Api.Infrastructure.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPosition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserPosition",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserPositionName",
+                table: "AspNetUsers",
+                type: "text",
+                unicode: false,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserPosition",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserPositionName",
+                table: "AspNetUsers");
+        }
+    }
+}
